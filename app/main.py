70f95c12d98102a5e4befc7f3ed4e69accab4593
madhu <madhu.kosuri@senecaglobal.com>
2025-07-19 23:33:33 +0530

    Refactor CORS middleware to improve formatting of allowed origins

--- app/main.py
+++ app/main.py
@@ -7,8 +7,14 @@ app = FastAPI(title="FastAPI Sample Skeleton")
 app.include_router(routes.router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://3.89.103.234/api/", "http://my-react-app-demo-july-18-2025.s3-website-us-east-1.amazonaws.com/", "https://d2tvxnyll1ff8k.cloudfront.net/"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://3.89.103.234",
+        "http://my-react-app-demo-july-18-2025.s3-website-us-east-1.amazonaws.com",
+        "https://d2tvxnyll1ff8k.cloudfront.net"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
