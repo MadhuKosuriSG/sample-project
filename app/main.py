640a04202c2e8948d48ede27107b11b8e3eb0fec
madhu <madhu.kosuri@senecaglobal.com>
2025-08-04 20:50:38 +0530

    Fix CORS middleware by updating an allowed origin IP address

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://3.89.103.234",
+        "http://34.227.90.56",
         "http://54.165.61.74:3000",
         "http://my-react-app-demo-july-18-2025.s3-website-us-east-1.amazonaws.com",
         "https://d2tvxnyll1ff8k.cloudfront.net"
